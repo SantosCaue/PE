ef16cb2ca18e3d67e9f1d23e9e597915988a539e
Cauê <132915276+SantosCaue@users.noreply.github.com>
2023-07-08 03:24:20 -0300

    comeco dos cookies

--- assets/entrar.js
+++ assets/entrar.js
@@ -11,7 +11,11 @@ function entrou() {
         var franca = document.getElementById("APRESENTA").getElementsByTagName("h2");
         for (let g = 0; g < franca.length; g++) {
             franca[g].style.display = "none";
+                    document.cookie = "loginStatus=" + cookieValue + "; expires=Thu, 31 Dec 2025 23:59:59 UTC; path=/";
+
         }
+        document.cookie = "loginStatus=" + "eiki" + "; expires=Thu, 31 Dec 2025 23:59:59 UTC; path=/";
+
         document.getElementById("resposta").innerHTML = "Seja muito bem vindo Gabriel Eiki";
     } else if (rm == 21290 && senha == "Ocuralesbica2.0") {
         document.getElementById("foto").src = "assets/riquelme.jpg";
@@ -24,12 +28,16 @@ function entrou() {
             franca[g].style.display = "none";
         }
         document.getElementById("resposta").innerHTML = "Seja muito bem vindo Riquelme Borges";
+        document.cookie = "loginStatus=" + "riquelme" + "; expires=Thu, 31 Dec 2025 23:59:59 UTC; path=/";
+
 
     } else if (rm == 21101 && senha == "Bressanfoda69") {
         document.getElementById("foto").src = "assets/bressan.jpg";
         document.login.RM.style.display = "none";
         document.login.Senha.style.display = "none";
         document.login.botao.style.display = "none";
+        document.cookie = "loginStatus=" + "bressan" + "; expires=Thu, 31 Dec 2025 23:59:59 UTC; path=/";
+
 
         var franca = document.getElementById("APRESENTA").getElementsByTagName("h2");
         for (let g = 0; g < franca.length; g++) {
@@ -48,6 +56,8 @@ function entrou() {
             franca[g].style.display = "none";
         }
         document.getElementById("resposta").innerHTML = "Seja muito bem vindo Cauê Santos";
+        document.cookie = "loginStatus=" + "caue" + "; expires=Thu, 31 Dec 2025 23:59:59 UTC; path=/";
+
     }else{
         rm = "";
         senha = "";
