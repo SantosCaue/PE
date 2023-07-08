32340cab85bcca03e190cdf1b7937098f8c9241a
Cauê <132915276+SantosCaue@users.noreply.github.com>
2023-07-08 18:30:32 -0300

    Aplicação dos cookies

--- assets/entrar.js
+++ assets/entrar.js
@@ -1,27 +1,40 @@
+function getCookie(name) {
+    var cookieArr = document.cookie.split("; ");
+    for (var i = 0; i < cookieArr.length; i++) {
+      var cookiePair = cookieArr[i].split("=");
+      if (name === cookiePair[0]) {
+        return cookiePair[1];
+      }
+    }
+    return null;
+  }
+
+  var loginStatus = getCookie("loginStatus");
+  
 function entrou() {
     var rm = document.login.RM.value;
     var senha = document.login.Senha.value;
 
-    if (rm == 21092 && senha == "intuitivoeamigavel23") {
+    if ((rm == 21092 && senha == "intuitivoeamigavel23") || loginStatus == "eiki") {
         document.getElementById("foto").src = "assets/eiki.jpg";
         document.login.RM.style.display = "none";
         document.login.Senha.style.display = "none";
         document.login.botao.style.display = "none";
+        document.getElementById("ss").style.display = "block";
 
         var franca = document.getElementById("APRESENTA").getElementsByTagName("h2");
         for (let g = 0; g < franca.length; g++) {
             franca[g].style.display = "none";
-                    document.cookie = "loginStatus=" + cookieValue + "; expires=Thu, 31 Dec 2025 23:59:59 UTC; path=/";
-
         }
         document.cookie = "loginStatus=" + "eiki" + "; expires=Thu, 31 Dec 2025 23:59:59 UTC; path=/";
 
         document.getElementById("resposta").innerHTML = "Seja muito bem vindo Gabriel Eiki";
-    } else if (rm == 21290 && senha == "Ocuralesbica2.0") {
+    } else if ((rm == 21290 && senha == "Ocuralesbica2.0") || loginStatus == "riquelme") {
         document.getElementById("foto").src = "assets/riquelme.jpg";
         document.login.RM.style.display = "none";
         document.login.Senha.style.display = "none";
         document.login.botao.style.display = "none";
+        document.getElementById("ss").style.display = "block";
 
         var franca = document.getElementById("APRESENTA").getElementsByTagName("h2");
         for (let g = 0; g < franca.length; g++) {
@@ -31,11 +44,12 @@ function entrou() {
         document.cookie = "loginStatus=" + "riquelme" + "; expires=Thu, 31 Dec 2025 23:59:59 UTC; path=/";
 
 
-    } else if (rm == 21101 && senha == "Bressanfoda69") {
+    } else if ((rm == 21101 && senha == "Bressanfoda69") || loginStatus == "bressan") {
         document.getElementById("foto").src = "assets/bressan.jpg";
         document.login.RM.style.display = "none";
         document.login.Senha.style.display = "none";
         document.login.botao.style.display = "none";
+        document.getElementById("ss").style.display = "block";
         document.cookie = "loginStatus=" + "bressan" + "; expires=Thu, 31 Dec 2025 23:59:59 UTC; path=/";
 
 
@@ -45,11 +59,12 @@ function entrou() {
         }
         document.getElementById("resposta").innerHTML = "Seja muito bem vindo Henrique Bressan";
 
-    }else if(rm == 20949 && senha == "senhaforte"){
+    } else if ((rm == 20949 && senha == "senhaforte") || loginStatus == "caue") {
         document.getElementById("foto").src = "assets/caue.jpg";
         document.login.RM.style.display = "none";
         document.login.Senha.style.display = "none";
         document.login.botao.style.display = "none";
+        document.getElementById("ss").style.display = "block";
 
         var franca = document.getElementById("APRESENTA").getElementsByTagName("h2");
         for (let g = 0; g < franca.length; g++) {
@@ -58,10 +73,25 @@ function entrou() {
         document.getElementById("resposta").innerHTML = "Seja muito bem vindo Cauê Santos";
         document.cookie = "loginStatus=" + "caue" + "; expires=Thu, 31 Dec 2025 23:59:59 UTC; path=/";
 
-    }else{
-        rm = "";
-        senha = "";
+    } else if ( rm != null && senha != null){
         document.getElementById("resposta").innerHTML = "Senha ou usuário incorretos. Tente novamente";
-
     }
 }
+
+function lambda(){
+    document.getElementById("foto").src = "assets/pfp.png";
+        document.login.RM.style.display = "block";
+        document.login.Senha.style.display = "block";
+        document.login.botao.style.display = "block";
+        document.getElementById("ss").style.display = "none";
+
+        var franca = document.getElementById("APRESENTA").getElementsByTagName("h2");
+        for (let g = 0; g < franca.length; g++) {
+            franca[g].style.display = "block";
+        }
+        document.getElementById("resposta").innerHTML = "";
+    document.cookie = "loginStatus=" + "null" + "; expires=Thu, 31 Dec 2025 23:59:59 UTC; path=/";
+}
+window.onload = function () {
+    entrou(); // Check login status when the page loads
+  }
--- assets/site.js
+++ assets/site.js
@@ -18,7 +18,7 @@ function site(nome, valor) {
 
 function computador(nome, valor){
         quantidade = quantidade + 1;
-        total = sum(total,valor,5)
+        total = sum(total,valor,5);
         item.push(nome);
         document.getElementById("ativo").innerHTML = quantidade;
         document.getElementById("ativo").style.backgroundColor = 'red';
@@ -29,5 +29,32 @@ function Ver(){
         alert("Seus itens são: " + item.join(", ") + ", o preço do seu carrinho é de: R$" + total);
 }else{
         alert("Seu carrinho está vazio, dê uma olhada nos nossos produtos ;)")
+}}
+
+function getCookie(name) {
+  var cookieArr = document.cookie.split("; ");
+  for (var i = 0; i < cookieArr.length; i++) {
+    var cookiePair = cookieArr[i].split("=");
+    if (name === cookiePair[0]) {
+      return cookiePair[1];
+    }
+  }
+  return null;
 }
+
+function checkLoginStatus() {
+  var loginStatus = getCookie("loginStatus");
+  if (loginStatus === "eiki") {
+    document.getElementById("foto").src = "assets/eiki.jpg";
+  } else if (loginStatus === "bressan") {
+    document.getElementById("foto").src = "assets/bressan.jpg";
+  } else if (loginStatus === "caue") {
+    document.getElementById("foto").src = "assets/caue.jpg";
+  } else if (loginStatus === "riquelme") {
+    document.getElementById("foto").src = "assets/riquelme.jpg";
+  }
+}
+
+window.onload = function () {
+  checkLoginStatus(); // Check login status when the page loads
 }
